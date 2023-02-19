5f18443d095869f41c84c1ad711fdfc6a63f64ca
0xDopamine <aminebumi1@gmail.com>
2023-02-19 19:22:18 +0100

    this still not working

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -1,9 +1,14 @@
 
 chown -R www-data:www-data /var/www/html/wordpress
+# mkdir /run/php/
+# touch /run/php/php7.3-fpm.sock
+# chown www-data:www-data /run/php/ -R
+# chmod 755 /run/php/
+# chmod 644 /run/php/php7.3-fpm.sock
+service php7.3-fpm start
+tail -f /dev/null
 # cp /var/www/html/wordpress/wp-config.php ./wordpress/wp-config-sample.php
 
-service php7.3-fpm restart
-service nginx restart
 
 
 
