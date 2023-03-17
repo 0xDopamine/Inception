f04392bd2e3743442c57f6970744a9becbb8004e
0xDopamine <aminebumi1@gmail.com>
2023-03-17 17:57:50 +0100

    done

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 DOCKER_COMPOSE_FILE=/Users/mbaioumy/Documents/Inception/srcs/docker-compose.yml
 DOCKER_COMPOSE_CMD=docker compose -f $(DOCKER_COMPOSE_FILE)
-SYSTEM_PRUNE=docker system prune
+SYSTEM_PRUNE=docker system prune -af
 
 all:
 	$(DOCKER_COMPOSE_CMD) up 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,9 +9,6 @@ cd /var/www/html/wordpress
 wp core download --allow-root
 wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
 
-chmod 600 wp-config.php
-chown www-data:www-data wp-config.php
-
-wp core install --url=https://localhost --title=DopamInception --admin_user=dopamine --admin_password=admin --admin_email=aminebumi1@gmail.com --allow-root
+wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root
 
 exec $@
