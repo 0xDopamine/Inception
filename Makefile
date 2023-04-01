f4a81b13b143c1299b0f8aa33ddbe84230ca86c3
0xDopamine <aminebumi1@gmail.com>
2023-04-01 18:34:51 +0000

    project done

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
-DOCKER_COMPOSE_FILE=/Users/mbaioumy/Documents/Inception/srcs/docker-compose.yml
-DOCKER_COMPOSE_CMD=docker compose -f $(DOCKER_COMPOSE_FILE)
+WP_PATH=/home/mbaioumy/data/wordpress
+DB_PATH=/home/mbaioumy/data/mariadb
+DATA_PATH=/home/mbaioumy/data
+DOCKER_COMPOSE_FILE=srcs/docker-compose.yml
+DOCKER_COMPOSE_CMD=docker-compose --file $(DOCKER_COMPOSE_FILE)
 SYSTEM_PRUNE=docker system prune -af
 
 all:
-	$(DOCKER_COMPOSE_CMD) up 
+	mkdir -p $(WP_PATH) $(DB_PATH)
+	$(DOCKER_COMPOSE_CMD) up -d
 clean:
 	$(DOCKER_COMPOSE_CMD) down
 fclean:
+	rm -rf $(DATA_PATH)
 	$(SYSTEM_PRUNE)
-	$(DOCKER_COMPOSE_CMD) down -v
+	$(DOCKER_COMPOSE_CMD) down -v --rmi all
 re: fclean all
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,8 +7,15 @@ chmod 660 /run/php/php7.3-fpm.sock
 
 cd /var/www/html/wordpress
 wp core download --allow-root
+
+
 wp core config --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
 
-wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root
+wp core install --url=${DOMAIN_NAME} --title=DopamInception --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PW} --admin_email=${WP_EMAIL} --allow-root
+
+wp user create ${WP_USER} random@gmail.com --user_pass=${WP_PW} --role=author --allow-root
+
+wp search-replace 'localhost' 'mbaioumy@42.fr' --allow-root
+wp search-replace 'https://localhost' 'https://mbaioumy@42.fr' --allow-root
 
 exec $@
